8656419adc3657346b072777e6cb4856124cb401
Jakey Fowler <jakey@pinecone.io>
2024-03-08 15:55:04 -0800

    .

--- embed.js
+++ embed.js
@@ -6,6 +6,7 @@ window.addEventListener(
       event.source.postMessage({action: 'request'}, event.origin)
     } else if (event.data.action === 'respond') {
       const val = event.data.value
+      console.log(val)
       await google.colab.kernel.invokeFunction('notebook.SetApiKey', [val], {})
     }
   }
