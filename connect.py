e38fe679892dad33db86729e86fb52a6d943388c
Jakey Fowler <jakey@pinecone.io>
2024-03-08 16:27:57 -0800

    .

--- connect.py
+++ connect.py
@@ -1,12 +1,12 @@
 from IPython.display import Javascript, display
 from google.colab import output
 
+API_KEY = ''
+
 def GetApiKey():
   def SetApiKey(val):
     global API_KEY
     API_KEY = val
-
-  SetApiKey('')
 
   output.register_callback('pinecone.SetApiKey', SetApiKey)
   display(Javascript(url='https://web-message-experiment.vercel.app/embed.js'))
