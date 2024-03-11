212650845f6bac5a9d8d08cf4e69266ed1cc6c94
Jakey Fowler <jakey@pinecone.io>
2024-03-11 12:27:57 -0700

    add auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ConfirmPage from './ConfirmPage'
 import MessageListener from './MessageListener'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MessageListener />} />
-        <Route path="confirm" element={<ConfirmPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Auth0Provider
+      domain="dev-jakey-experiment.us.auth0.com"
+      clientId="bZXPbfTQWl9D9E8LhtTucubUe6ng3MZs"
+      authorizationParams={{
+        redirect_uri: 'https://web-message-experiment.vercel.app/confirm'
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MessageListener />} />
+          <Route path="confirm" element={<ConfirmPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Auth0Provider>
   )
 }
 
