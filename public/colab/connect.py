2241c93d4374b49644255d2932595a98f6485e6c
Jakey Fowler <jakey@pinecone.io>
2024-03-18 18:08:33 -0700

    .

--- public/colab/connect.py
+++ public/colab/connect.py
@@ -1,9 +1,6 @@
 from IPython.display import Javascript, display
 from google.colab import output
 
-API_KEY = ''
-
 def ConnectToPinecone():
-  global API_KEY
   display(Javascript(url='https://web-message-experiment.vercel.app/colab/embed.js'))
-  API_KEY = output.eval_js('connectToPinecone')
+  return output.eval_js('connectToPinecone')
