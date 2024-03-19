01731203126157e0ebfa730f85904ad04c78d575
Jakey Fowler <jakey@pinecone.io>
2024-03-19 09:49:44 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './AppRoutes'
 import AuthProvider from './AuthProvider'
 import MessageProvider from './messages/MessageProvider'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    window.addEventListener("message", (event) => window.alert(event))
+  }, [])
+
   return (
     <MessageProvider>
       <BrowserRouter>
